1f641aa2e9b0ea149c3f280e7c845c7835e91fc0
NT712 <97837270+NT712@users.noreply.github.com>
2022-02-02 15:10:03 +0700

    Add files via upload

---
+++ assets/preload/stages/night.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('night', 'night', -300, -100);
+	setLuaSpriteScrollFactor('night', 1.0, 1.0);
+
+	addLuaSprite('night', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
